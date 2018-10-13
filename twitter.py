63f412b2853f9d9a75ef1928e152e1b0c2c0dd67
Ryan Siu <rsiu05@gmail.com>
2018-10-13 01:03:34 -0400

    Add Twitter API handling

---
+++ twitter.py
@@ -0,0 +1,58 @@
+from config import TWITTER_API_KEY, TWITTER_API_SECRET_KEY
+
+import base64
+import requests
+import json
+import pprint
+
+def get_credentials():
+    return TWITTER_API_KEY + ':' + TWITTER_API_SECRET_KEY
+
+def encode_base64(s):
+    return base64.b64encode(s)
+
+def get_bearer_token():
+    token = encode_base64(get_credentials())
+    r = requests.post('https://api.twitter.com/oauth2/token',
+                     headers={
+                         'Authorization': 'Basic ' + token,
+                         'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                     },
+                     data='grant_type=client_credentials'
+    )
+
+    r = r.json()
+    assert r['token_type'] == 'bearer'
+    return 'Bearer ' + r['access_token']
+
+def parse_tweets(json):
+    tweets = {}
+    for tweet in json['statuses']:
+        tweets[tweet['id']] = {
+            'text': tweet['text'].encode('ascii','ignore'),
+            'created': tweet['created_at'],
+            'retweet_count': tweet['retweet_count'],
+            'favorite_count': tweet['favorite_count']
+        }
+
+    return tweets
+
+def search(query, n, result_type):
+    assert n > 0 and n <= 100
+    assert result_type == 'recent' or result_type == 'mixed' or result_type == 'popular'
+
+    params = {'q': query, 'result_type': result_type, 'count': str(n), 'lang': 'en'}
+    r = requests.get('https://api.twitter.com/1.1/search/tweets.json',
+                     params=params,
+                     headers={
+                         'User-Agent': 'Augury MHacks',
+                         'Authorization': get_bearer_token()
+                     }
+    )
+
+    return parse_tweets(r.json())
+
+def main():
+    pprint.pprint(search('"tesla" OR "Tesla"', 100, 'recent'))
+
+main()
