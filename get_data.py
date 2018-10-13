9b642282c640007ed0cf22755d939d9c331e58c9
Ryan Siu <rsiu05@gmail.com>
2018-10-13 19:28:02 -0400

    Add example get data script

---
+++ get_data.py
@@ -0,0 +1,20 @@
+from config import FIRESTORE_CERT
+
+import pprint
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+def main():
+    cred = credentials.Certificate(FIRESTORE_CERT)
+    firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
+    docs = db.collection('companies').get()
+
+    for doc in docs:
+        print(doc)
+        pprint(doc.to_dict())
+
+main()
